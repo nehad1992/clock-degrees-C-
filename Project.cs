ea825feeac7d612c6f63f5a6f43febb5d9c62e13
neha doodi and Sasa Schwartz <sasa@>
2020-02-24 13:47:02 -0800

    Add method to calculate degrees

--- Project.cs
+++ Project.cs
@@ -3,6 +3,19 @@ using System.Collections.Generic;
 
 class Clock {
   static void Main () {
-    // project code goes here
+    // 1 hr is 30 degrees, 1 minute is 0.5 degrees
+    Console.WriteLine ("Enter the hour value");
+    int hours = int.Parse (Console.ReadLine ());
+    int hoursDegree = hours * 30;
+    Console.WriteLine ("Enter the minute value");
+    int minutes = int.Parse (Console.ReadLine ());
+    int minutesDegree = minutes * 6;
+    int subtractionValue = Math.Abs ((hoursDegree - minutesDegree));
+    if (subtractionValue > 180) {
+      int complementValue = 360 - subtractionValue;
+      Console.WriteLine (complementValue);
+    } else {
+      Console.WriteLine (subtractionValue);
+    }
   }
 }
